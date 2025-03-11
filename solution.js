9d86bd94fc3aeb6fce250fe428d42da8214ca309
Dhashwathi M B <dhashwathi.mb2021ai@sece.ac.in>
2025-03-11 14:49:55 +0530

    Basic arithemetic functions for two numbers

--- solution.js
+++ solution.js
@@ -1,4 +1,10 @@
-var first_number=10;
-var second_number=20;
+var first_number=20;
+var second_number=10;
 var sum=first_number+second_number;
+var subtract=first_number-second_number;
+var multiply=first_number*second_number;
+var division=first_number/second_number;
 console.log(sum);
+console.log(subtract);
+console.log(multiply);
+console.log(division);
